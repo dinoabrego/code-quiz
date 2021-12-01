1d9a35e1103c50cd77c7f8cdd5ed7aa21dbe665b
Dinorah Abrego <dinoabrego@Dinorahs-MacBook-Air.local>
2021-11-30 19:09:33 -0600

    Updated styles. Added list of questions for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var startButton = document.getElementById('start-button');
 var startContainer = document.getElementById('start-container');
 var questionsContainer = document.getElementById('question-container'); 
+var endContainer = document.getElementById('end-container'); 
 var questionEl = document.getElementById('questions');
 var answerEl = document.getElementById('answer-btn');
 var nextButton = document.getElementById('next-button');
@@ -9,11 +10,22 @@ var currentQuestion;
 var score = 0;
 var button;
 
+//Variables for Timer
+var timerEl = document.getElementById('timer-count');
+var timer;
+var timerCount;
 
-//Starts the Game
+
+//This event starts the game
 startButton.addEventListener('click', startQuiz);
 
-//Hides initial instructions, then selects a question to show
+//This event changes questions
+nextButton.addEventListener('click', () => {
+    currentQuestion++;
+    nextQuestion();
+})
+
+//Hides initial instructions, then selects a question to show and starts the timer count
 function startQuiz () {
     console.log('Quiz is starting');
     startContainer.classList.add('hidden');
@@ -21,6 +33,11 @@ function startQuiz () {
     currentQuestion= 0;
     questionsContainer.classList.remove('hidden');
     nextQuestion();
+    startTimer()
+}
+
+// 
+function startTimer(){
 
 }
 
@@ -58,7 +75,12 @@ function selectAnswer (e) {
    Array.from(answerEl.children).forEach(button => {
        checkAnswer(button, button.dataset.correct);
    })
-   nextButton.classList.remove('hidden');
+   if (randomQuestion.length > currentQuestion +1) {
+    nextButton.classList.remove('hidden');
+   }
+   else {
+       showHighscore()
+   }
 }
 
 //Checks if the answer selected is right or wrong, changes the visual look of it, and takes time off the timer 
@@ -79,8 +101,23 @@ function clearAnswer (element) {
 
 }
 
+function showHighscore () {
+    questionsContainer.classList.add('hidden');
+    endContainer.classList.remove('hidden');
+}
+
+//Array of questions for the quiz
 
 var questions = [
+    {
+        question: 'Which type of language is JavaScript?',
+        answers: [
+            {text: 'Object-Oriented', correct:false},
+            {text: 'Object-Based', correct:true},
+            {text: 'Assembly-language', correct:false},
+            {text: 'High-level', correct:false},
+        ]
+    },
     {
         question: 'Commonly used data types DO NOT include:',
         answers: [
@@ -89,5 +126,32 @@ var questions = [
             {text: 'Alerts', correct:true},
             {text: 'Numbers', correct:false},
         ]
-    }
+    },
+    {
+        question: 'The condition inside an if/else statement is enclosed within',
+        answers: [
+            {text: 'Curly Brackets {} ', correct:true},
+            {text: 'Quotes "" ', correct:false},
+            {text: 'Parenthesis () ', correct:false},
+            {text: 'Square Brackets [] ', correct:false},
+        ]
+    },
+    {
+        question: 'Arrays can be used to store',
+        answers: [
+            {text: 'Strings', correct:false},
+            {text: 'Numbers', correct:false},
+            {text: 'Other arrays', correct:false},
+            {text: 'All the above', correct:true},
+        ]
+    },
+    {
+        question: 'Strings should be within _____ when being assigned to variables',
+        answers: [
+            {text: 'Parenthesis () ', correct:false},
+            {text: 'Square Brackets [] ', correct:false},
+            {text: 'Quotes "', correct:true},
+            {text: 'Curly Brackets {} ', correct:false},
+        ]
+    },
 ]
